fed4368090c77dff07d2c8216b1468c5a06a9aac
jmunsch <james.a.munsch@gmail.com>
2015-05-28 14:14:00 -0500

    added comments on how react parent and children work

--- src/js/Child.js
+++ src/js/Child.js
@@ -2,6 +2,8 @@ var Child = React.createClass({
   render: function(){
     return (
       <div>
+        // this.props is a built in
+        // this.props has properties as set by the Parent.js
         and this is the <b>{this.props.name}</b>.
       </div>
     )
--- src/js/Parent.js
+++ src/js/Parent.js
@@ -3,7 +3,15 @@ var Parent = React.createClass({
     return (
       <div>
         <div> This is the parent. </div>
+        // Here we call Child.js the Child class
         <Child name="child"/>
+          //****************************//
+	      // <div>
+	      //   // this.props is a built in
+	      //   // this.props has properties as set by the Parent.js
+	      //   and this is the <b>{this.props.name}</b>.
+	      // </div>
+	      //***************************//
       </div>
     )
   }
