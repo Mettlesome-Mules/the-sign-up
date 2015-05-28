767069f085704df2b4185991b1ab79799ab978ee
Justin Hernandez <jmarthernandez@gmail.com>
2015-05-28 15:05:53 -0500

    updated gulpfile with plugs/routes

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,79 @@
-empty files not tracked
+// Include gulp
+var gulp = require('gulp'); 
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var sass  = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var mocha = require('gulp-mocha');
+ 
+
+var path = {
+  HTML: 'src/index.html',
+  ALL: ['src/js/*.js', 'src/js/**/*.js', 'src/index.html'],
+  JS: ['src/js/*.js', 'src/js/**/*.js'],
+  MINIFIED_OUT: 'build.min.js',
+  DEST_SRC: 'dist/src',
+  DEST_BUILD: 'dist/build',
+  DEST: 'dist'
+};
+
+gulp.task('transform', function(){
+  gulp.src(path.JS)
+    .pipe(react())
+    .pipe(gulp.dest(path.DEST_SRC));
+});
+
+gulp.task('copy', function(){
+  gulp.src(path.HTML)
+    .pipe(gulp.dest(path.DEST));
+});
+
+gulp.task('watch', function(){
+  gulp.watch(path.ALL, ['transform', 'copy']);
+});
+
+gulp.task('build', function(){
+  gulp.src(path.JS)
+    .pipe(react())
+    .pipe(concat(path.MINIFIED_OUT))
+    .pipe(uglify(path.MINIFIED_OUT))
+    .pipe(gulp.dest(path.DEST_BUILD));
+});
+
+gulp.task('replaceHTML', function(){
+  gulp.src(path.HTML)
+    .pipe(htmlreplace({
+      'js': 'build/' + path.MINIFIED_OUT
+    }))
+    .pipe(gulp.dest(path.DEST));
+});
+
+// ***************************** //
+// https://www.npmjs.com/package/gulp-jshint
+// ***************************** //
+gulp.task('lint', function() {
+  return gulp.src('./lib/*.js')
+    .pipe(jshint())
+    // Info on reporters
+    // https://www.npmjs.com/package/gulp-jshint#reporters
+    .pipe(jshint.reporter('default'));
+});
+
+
+// ***************************** //
+// https://www.npmjs.com/package/gulp-mocha
+// ***************************** //
+gulp.task('mocha', function () {
+    return gulp.src('spec/test.js', {read: false})
+        // gulp-mocha needs filepaths so you can't have any plugins before it 
+        .pipe(mocha({reporter: 'nyan'}));
+});
+
+
+gulp.task('default', ['watch']);
+
+gulp.task('production', ['replaceHTML', 'build']);
+
